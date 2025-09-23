e555e076791a244435e0d33869833e08c9e64bfb
Ashwiniya <ashwiniya60@gmail.com>
2025-09-23 11:03:00 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main{
+    /**
+     * main is where every execution starts
+     * @param args the arguments  not given
+     *  initatizes student details and print
+     */
+    public static  void main(String[] args) {
+        // initialising name rollno,college//
+        final String name="ASHWINIYA"; //the keyword final is given so name cannot be changed//
+        int rollno=14;
+        String college="KPRIET";
+        // prints studentdetails//
+        System.out.println(name);
+        System.out.println(rollno);
+        System.out.println(college);
+    }
+}
